b89b39b7ffc0ae18727e86e2394e57c97dff591d
Le Ngoc Hoan <le-ngoc-hoan@altplus.co.jp>
2016-07-21 11:15:40 +0900

    Initial Commit

---
+++ RealmTraining/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmTraining
+//
+//  Created by le-ngoc-hoan on 2016/07/21.
+//  Copyright © 2016年 Le Ngoc Hoan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
