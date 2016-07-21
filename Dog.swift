9bce7a93ebf4b2ec99066fec6b9c6d2f07ebd86f
Le Ngoc Hoan <le-ngoc-hoan@altplus.co.jp>
2016-07-21 11:28:33 +0900

    Init project

---
+++ Dog.swift
@@ -0,0 +1,19 @@
+//
+//  Dog.swift
+//  
+//
+//  Created by le-ngoc-hoan on 2016/07/21.
+//
+//
+
+import Foundation
+import RealmSwift
+
+class Dog: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
